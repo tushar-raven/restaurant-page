ccd8cedda79446a9145febb2e477c65d7d24e81d
tusharravenclaw <tushar68053@gmail.com>
2023-03-24 22:59:08 +0530

    add animation

--- src/website.js
+++ src/website.js
@@ -28,26 +28,47 @@ function createHeader() {
   const homeBtn = document.createElement("button");
   homeBtn.className = "nav-button";
   homeBtn.textContent = "Home";
+  homeBtn.classList.add("active");
   navBox.appendChild(homeBtn);
-  homeBtn.addEventListener("click", loadHome);
+  homeBtn.addEventListener("click", (e) => {
+    setAnimation(e);
+    loadHome();
+  });
 
   const menuBtn = document.createElement("button");
   menuBtn.className = "nav-button";
   menuBtn.textContent = "Menu";
   navBox.appendChild(menuBtn);
-  menuBtn.addEventListener("click", loadMenu);
+  menuBtn.addEventListener("click", (e) => {
+    setAnimation(e);
+    loadMenu();
+  });
 
   const contactBtn = document.createElement("button");
   contactBtn.className = "nav-button";
   contactBtn.textContent = "Contact";
   navBox.appendChild(contactBtn);
-  contactBtn.addEventListener("click", loadContact);
+  contactBtn.addEventListener("click", (e) => {
+    setAnimation(e);
+    loadContact();
+  });
+
+  return homeBtn;
 }
 
 function createMain() {
   const mainBox = document.createElement("div");
   mainBox.className = "main-box";
   content.appendChild(mainBox);
+}
+
+function setAnimation(e) {
+  const navButtons = document.querySelectorAll(".nav-button");
+  navButtons.forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  e.target.classList.add("active");
 }
 
 export default initialWebsite;
